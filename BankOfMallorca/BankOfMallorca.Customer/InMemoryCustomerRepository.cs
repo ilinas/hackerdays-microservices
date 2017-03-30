a426fb2067d229f952142b4046289c679fc63d33
Jacob Eisenberg <jei@trifork.com>
2017-03-30 12:33:00 +0200

    Added Events module and an in-memory event store

--- BankOfMallorca/BankOfMallorca.Customer/InMemoryCustomerRepository.cs
+++ BankOfMallorca/BankOfMallorca.Customer/InMemoryCustomerRepository.cs
@@ -6,11 +6,22 @@ namespace BankOfMallorca.Customer
 {
     public class InMemoryCustomerRepository : ICustomerRepository
     {
+        private readonly IEventStore _eventStore;
         private static readonly IDictionary<Guid, Customer> Customers = new ConcurrentDictionary<Guid, Customer>();
+
+        public InMemoryCustomerRepository(IEventStore eventStore)
+        {
+            _eventStore = eventStore;
+        }
 
         public void Save(Customer customer)
         {
             Customers.Add(customer.Id, customer);
+            _eventStore.Raise("CustomerCreated", new
+            {
+                customer.Id,
+                customer.Name
+            });
         }
     }
 }
---
+++ BankOfMallorca/BankOfMallorca.Customer/InMemoryEventStore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BankOfMallorca.Customer
+{
+    public class InMemoryEventStore : IEventStore
+    {
+        private static readonly IList<Event> Events = new List<Event>();
+
+        public void Raise(string eventName, object payload)
+        {
+            var seqNo = Events.Count;
+            Events.Add(new Event(seqNo, eventName, payload));
+        }
+
+        public IEnumerable<Event> GetRange(int start, int end)
+        {
+            return Events.Skip(start).Take(end - start);
+        }
+    }
+}
